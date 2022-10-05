4868666ac23edec7ee71bf8cae3977fb06b6e2e6
rikiachmad <rikiachmad@yahoo.com>
2022-10-05 12:16:33 +0700

    feat: add send/receive functionality

--- client.py
+++ client.py
@@ -3,14 +3,18 @@ import sys, os
 from tqdm import tqdm
 
 class Client():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SEPARATOR = "<SEPARATOR>"
     BUFFER_SIZE = 4096
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     def __init__(self, host="127.0.0.1", port=65432):
         self.host = host
         self.port = port
         self.s.connect((host, port))
     
     def send_file(self, filename):
+        self.s.send(f"{filename}{self.SEPARATOR}{filesize}".encode())
+
         filesize = os.path.getsize(filename)
         progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
         
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-import socket, logging
+import socket, logging, os
+from tqdm import tqdm
 
 class Server:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SEPARATOR = "<SEPARATOR>"
     BUFFER_SIZE = 4096
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     def __init__(self, host="0.0.0.0", port=65432):
         self.host = host
         self.port = port
@@ -11,6 +14,28 @@ class Server:
     def receive_file(self):
         self.s.listen(5)
         logging.info((f"Listening as {self.host}:{self.port}..."))
+        client_socket, address = self.s.accept()
+        logging.info(f"{address} is connected.")
+
+        received = client_socket.recv(self.BUFFER_SIZE).decode()
+        filename, filesize = received.split(self.SEPARATOR)
+
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            while True:
+                bytes_read = client_socket.recv(self.BUFFER_SIZE)
+                if not bytes_read:    
+                    break
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+
+        # close the client socket
+        client_socket.close()
+        # close the server socket
+        self.s.close()
 
     def get_host(self):
         return self.host
